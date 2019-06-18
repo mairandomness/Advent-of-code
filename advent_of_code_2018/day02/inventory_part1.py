62dd6a4be0e3533899cce57c0ba283a110572383
mairandomness <mairakodama@gmail.com>
2019-06-18 20:54:36 -0300

    add day02

---
+++ advent_of_code_2018/day02/inventory_part1.py
@@ -0,0 +1,31 @@
+def parse_input():
+    with open("input", "r") as f:
+        text = f.read()[:-1]
+    lines = text.split("\n")
+    return lines
+
+
+def checksum(lines):
+    two_times = [0]*len(lines)
+    three_times = [0]*len(lines)
+
+    for i, line in enumerate(lines):
+
+        for letter in line:
+
+            if line.count(letter) == 2:
+                two_times[i] = 1
+
+            if line.count(letter) == 3:
+                three_times[i] = 1
+    
+    return sum(two_times) * sum(three_times)
+
+
+def main():
+    lines = parse_input()
+    print(checksum(lines))
+
+
+if __name__ == "__main__":
+    main()
---
+++ advent_of_code_2018/day02/inventory_part2.py
@@ -0,0 +1,43 @@
+def parse_input():
+    with open("input", "r") as f:
+        text = f.read()[:-1]
+    lines = text.split("\n")
+    return lines
+
+
+def box_id(lines):
+    two_times = [0]*len(lines)
+    three_times = [0]*len(lines)
+
+    for word1 in lines:
+        for word2 in lines:
+            if word1 > word2:
+                diff = 0
+
+                for i, letter in enumerate(word1):
+
+                    if letter != word2[i]:
+                        diff += 1
+
+                        if diff > 1:
+                            break
+
+                if diff == 1:
+                    # we found the words, now its just about convenient printing
+                    print(word1, word2)
+
+                    for i, letter in enumerate(word1):
+                        
+                        if letter == word2[i]:
+                            print(letter, end='')
+                    break
+
+
+def main():
+    lines = parse_input()
+    box_id(lines)
+    print()
+
+
+if __name__ == "__main__":
+    main()
