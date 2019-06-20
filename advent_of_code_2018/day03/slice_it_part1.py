4e7c5c1c28397353d828d1350792b29319d8fe9c
mairandomness <mairakodama@gmail.com>
2019-06-19 23:41:54 -0300

    add day03

---
+++ advent_of_code_2018/day03/slice_it_part1.py
@@ -0,0 +1,38 @@
+def parse_input():
+    with open("input", "r") as f:
+        text = f.read()[:-1]
+    lines = text.split("\n")
+    lines = [line.split(" ") for line in lines]
+    lines = [[line[2][:-1].split(','), line[3].split('x')]for line in lines]
+    lines = [[[int(num) for num in coord] for coord in line]for line in lines]
+    return lines
+
+
+def count_claims(fabric, claims):
+    for claim in claims:
+        line_start = claim[0][1]
+        row_start = claim[0][0]
+        line_end = line_start + claim[1][1]
+        row_end = row_start + claim[1][0]
+
+        for i in range(line_start, line_end):
+            for j in range(row_start, row_end):
+                fabric[i][j] += 1
+
+    count = 0
+    for line in fabric:
+        for item in line:
+            if item > 1:
+                count += 1
+    #print('\n'.join(['\t'.join([str(cell) for cell in row[:10]]) for row in fabric[:10]]))
+    return count
+
+
+def main():
+    fabric = [[0] * 1000 for n in range(1000)]
+    claims = parse_input()
+    print(count_claims(fabric, claims))
+
+
+if __name__ == "__main__":
+    main()
---
+++ advent_of_code_2018/day03/slice_it_part2.py
@@ -0,0 +1,44 @@
+def parse_input():
+    with open("input", "r") as f:
+        text = f.read()[:-1]
+    lines = text.split("\n")
+    lines = [line.split(" ") for line in lines]
+    lines = [[line[2][:-1].split(','), line[3].split('x')]for line in lines]
+    lines = [[[int(num) for num in coord] for coord in line]for line in lines]
+    return lines
+
+
+def count_claims(fabric, claims):
+    ids = set([i for i in range(1, len(claims) + 1)])
+    
+    for id1, claim1 in enumerate(claims):
+        for id2, claim2 in enumerate(claims):
+            if claim1[0] < claim2[0]:
+
+                line1_start = claim1[0][1]
+                row1_start = claim1[0][0]
+                line1_end = line1_start + claim1[1][1]
+                row1_end = row1_start + claim1[1][0]
+
+                line2_start = claim2[0][1]
+                row2_start = claim2[0][0]
+                line2_end = line2_start + claim2[1][1]
+                row2_end = row2_start + claim2[1][0]
+
+                if not ((line1_start > line2_end or line2_start > line1_end) or (row1_start > row2_end or row2_start > row1_end)):
+                    ids.discard(id1 + 1)
+                    ids.discard(id2 +1)
+                    # print("pair")
+                    # print(claim1)
+                    # print(claim2)
+
+    return ids
+
+
+def main():
+    claims = parse_input()
+    print(count_claims(fabric, claims))
+
+
+if __name__ == "__main__":
+    main()
