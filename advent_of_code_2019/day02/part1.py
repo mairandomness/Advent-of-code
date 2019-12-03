c03326084a32c835ae40627789205916eb9a0ed9
mairandomness <mairakodama@gmail.com>
2019-12-02 23:03:49 -0800

    day02

---
+++ advent_of_code_2019/day02/part1.py
@@ -0,0 +1,33 @@
+def parse_input():
+    with open("input", "r") as f:
+        text = f.read()[:-1]
+        lines = text.split(",")
+    return [int(line) for line in lines]
+
+def run_code(numbers):
+    numbers[1] = 12
+    numbers[2] = 2
+    i = 0
+    print(numbers)
+    while True:
+        if numbers[i] == 99:
+            break
+
+        elif numbers[i] == 1:
+            numbers[numbers[i+3]] = numbers[numbers[i+1]] + numbers[numbers[i+2]]
+            i += 4
+
+        elif numbers[i] == 2:
+            numbers[numbers[i+3]] = numbers[numbers[i+1]] * numbers[numbers[i+2]]
+            i += 4
+        if i == 4:
+            print(numbers)
+    return numbers[0]
+
+def main():
+    numbers = parse_input()
+    
+    print(run_code(numbers))
+
+if __name__ == "__main__":
+    main()
---
+++ advent_of_code_2019/day02/part2.py
@@ -0,0 +1,39 @@
+def parse_input():
+    with open("input", "r") as f:
+        text = f.read()[:-1]
+        lines = text.split(",")
+    return [int(line) for line in lines]
+
+def test_result(numbers):
+    numbers_copy = numbers[:]
+    for numbers[1] in range(100):
+        for numbers[2] in range(100):
+            noun = numbers[1]
+            verb = numbers[2]
+            numbers = numbers_copy[:]
+            numbers[1] = noun
+            numbers[2] = verb
+            i = 0
+
+            while True:
+                if numbers[i] == 99:
+                    break
+
+                elif numbers[i] == 1:
+                    numbers[numbers[i+3]] = numbers[numbers[i+1]] + numbers[numbers[i+2]]
+                    i += 4
+
+                elif numbers[i] == 2:
+                    numbers[numbers[i+3]] = numbers[numbers[i+1]] * numbers[numbers[i+2]]
+                    i += 4
+            if numbers[0] == 19690720:
+                return 100 * noun + verb 
+    
+
+def main():
+    numbers = parse_input()
+    
+    print(test_result(numbers))
+
+if __name__ == "__main__":
+    main()
