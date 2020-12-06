8c06d74585a23ea979e8944b33e783f432d04e5d
mairandomness <mairandomness@gmail.com>
2020-12-05 21:14:02 -0800

    add day06 solutions

---
+++ advent_of_code_2020/day06/part2.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+def parse_input():
+    with open("input", "r") as f:
+        text = f.read()
+    data = text.split("\n\n")
+    groups = [group.split("\n") for group in data]
+    return groups
+
+
+def find_yesses(groups):
+    acc_yesses = []
+    for group in groups:
+        yesses = set(group[0])
+        for person in group:
+            yesses = yesses.intersection(set(person))
+        acc_yesses.append(len(yesses))
+    return sum(acc_yesses)
+
+
+if __name__ == "__main__":
+    groups = parse_input()
+    print(find_yesses(groups))
